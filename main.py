a3c2293e7be34781f1f90ab59af28bd4307ff2ac
matveyka-voron <matveyka.voron@yandex.ru>
2020-03-07 15:26:21 +0700

    main Python code file for project

---
+++ main.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from PIL import *
+import os, time, webbrowser
+
+root = Tk()
+root.geometry('500x400')
+root.title('FilRed')
+root.resizable(width=False, height=False)
+# window.iconbitmap('files\\icon.ico')
+
+root.mainloop()
+
+
